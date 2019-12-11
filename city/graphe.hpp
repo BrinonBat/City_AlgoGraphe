f858a1a569f52e29a807a1de28855fc3a2e3a884
pdelepine <delepine.py@gmail.com>
2019-12-11 22:18:16 +0100

    dossier city

---
+++ city/graphe.hpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <tuple>
+#include <array>
+#include <limits>
+
+const int Max = 1000;
+
+class Graphe{
+  private : 
+    std::array<std::array<double,Max>, Max> matrice;
+    std::array<bool,Max> parcourus;
+    int nbSommets;
+    bool oriente;
+    
+  public :
+
+    Graphe();
+    ~Graphe();
+    void initMatrice(int nbSommets);
+    int getNbSommets(int i) {return nbSommets;}
+    void setOriente(bool o) {oriente = o;};
+    void ajoutArc(int,int,double); 
+    void affichageMatrice(); 
+    void parcoursProfondeur();
+    void explorer(int s);
+    void parcoursLargeur();
+    void dijkstra(int src);
+    double minDistance(std::array<double, Max> dist, std::array<bool, Max> sptSet);
+    void printSolution(std::array<double, Max> dist);
+    void fordfulkerson(int deb, int fin);
+};
